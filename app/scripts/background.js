6b83792ebf8701345c028fe5738a8970e5985748
Abdelhak Akermi <abdelhak.akermi@gmail.com>
2020-03-20 22:17:33 +0300

    refactor

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,8 +2,10 @@ browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-browser.browserAction.setBadgeText({
-  text: `'Allo`
-})
-
+// browser.browserAction.setBadgeText({
+//   text: `'Allo`
+// })
+// browser.browserAction.setBadgeBackgroundColor({
+//   color: `red`
+// })
 console.log(`'Allo 'Allo! Event Page for Browser Action`)
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,2 @@
-console.log(`'Allo 'Allo! Popup`)
+
+console.log("popup script started..");
