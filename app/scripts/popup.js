4504f4e0e21fe6b06646ed60aeccbd2ee87da718
Abdelhak Akermi <abdelhak.akermi@gmail.com>
2020-03-25 16:21:42 +0300

    refactor

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,19 +1,23 @@
 
-console.log("popup script started..");
-let element=document.getElementById("_element")
-element.addEventListener('click',function(evt){
 
-    chrome.tabs.captureVisibleTab(null, {format:"jpeg"}, function(result){
-    //TODO save image to clipboard
-    
-    //send notification about image
-    chrome.notifications.create("id", {type:"basic" ,iconUrl:"../images/notification.png",
-    title:"Notification",message:"copied ;) !"}, function (e){
-   chrome.tabs.create({url:"../pages/capture.html"},function(e){
+let element
+let currentTabTitle
 
-   })
+
+
+init()
+
+function init() {
+    chrome.tabs.query({active:true},function(tabs) {
+        currentTabTitle=tabs[0].title
+       element=document.getElementById("_element")
+       addListener()
+         });
+  }
+
+
+function addListener(){
+    element.addEventListener('click',function(evt){
+        handleCapture(currentTabTitle)
     })
-    
-
-    })
-})
+}
